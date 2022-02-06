8c43f977f8406cb65e46727ebe9e59d0805f6be9
JossFranco <jossfranco70@gmail.com>
2022-02-05 20:55:44 -0500

    avance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+}
+function mostrarSection2(){
+document.getElementById("section1").style.display = "none";
+document.getElementById("section2").style.display = "block";
+}

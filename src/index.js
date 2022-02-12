3950f39e3e64f8ffc1a3be5c57f8d141f214a4fb
JossFranco <jossfranco70@gmail.com>
2022-02-12 11:06:45 -0500

    avances

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import validator from './validator.js';
-
-console.log(validator);
+//import validator from "./validator.js";
 
 
-}
-function mostrarSection2(){
-document.getElementById("section1").style.display = "none";
-document.getElementById("section2").style.display = "block";
+document.getElementById("btn").addEventListener("click", capture);
+function capture() {
+
+    let dato = document.getElementById("numberCard").value;
+
+    if (dato == "") {
+        alert("Ingrese el número de tarjeta");
+    }
+    else {
+        let prueba = document.getElementById("prueba");
+        prueba.innerHTML = dato;
+
+        document.getElementById("pruebas").style.display = "none";
+        document.getElementById("prueba").style.display = "block";
+    }
 }

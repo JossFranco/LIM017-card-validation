7c9956882d7f05779bb406e308779e9ac1a00afa
JossFranco <jossfranco70@gmail.com>
2022-02-21 20:09:14 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,40 @@
+import validator from "./validator.js";
+
+const enterBtn = document.querySelector("#btn");
+
+    enterBtn.addEventListener("click", () => {
+    
+    const numberCard = document.getElementById("numberCard").value;
+    const message = document.getElementById("message");
+    const maskOfCard = document.getElementById("maskOfCard");
+    let finalAnswer = validator.isValid(numberCard);
+    let maskAnswer= validator.maskify(numberCard);
+
+    if (numberCard === "") {
+        let phrase=document.getElementById("phrase");
+        phrase.innerHTML='Por favor no olvide ingresar el número de la tarjeta';
+    } 
+    else {
+        document.getElementById("firstSection").style.display = "none";
+        document.getElementById("secondSection").style.display = "block";
+        if(finalAnswer == true){
+            maskOfCard.innerHTML=maskAnswer;
+            message.innerHTML='';
+            message.innerHTML= "Tarjeta valida. Gracias por su preferencia";
+        }
+        else{
+            maskOfCard.innerHTML=maskAnswer;
+            message.innerHTML='';
+            message.innerHTML= "Su tarjeta no es valida, intentelo nuevamente";
+        }
+    }    
+     return finalAnswer; 
+})
+
+const enterBtn1 = document.querySelector("#btn1");
+
+    enterBtn1.addEventListener("click", () => {
+        document.getElementById("firstSection").style.display = "block";
+        document.getElementById("secondSection").style.display = "none";
+
+    })
---
+++ validator.js
@@ -0,0 +1,49 @@
+const validator = {
+  isValid: function (x) {
+    //Convertir el número de tarjeta en array
+    let arrayCard = Array.from(x);
+    //Convertir los strings del array a números enteros
+    let secondArray = []; //Cree un array vacío
+    for (let i = 0; i < arrayCard.length; i++) {
+      //Cree un bucle para almacenar los strings del anterior array convertidos en números
+      secondArray.push(parseInt(arrayCard[i]));
+    }
+    //Invertir los números del array
+    secondArray.reverse();
+    //Ubicar los números de posiciones "pares"
+    let sumaPares = 0;
+    let sumaImpares = 0;
+    for (let i = 1; i < secondArray.length; i+=2) {
+      //Multiplicar por 2 los números encontrados del array
+      let positionPar = secondArray[i]*2;
+      if (positionPar >= 10) {
+        sumaPares = sumaPares + 1 + positionPar % 10;
+      }
+      else sumaPares = sumaPares + positionPar;
+    }
+    for (let i = 0; i < secondArray.length; i+=2) {
+      sumaImpares = sumaImpares + secondArray[i];
+    }
+    let sumaTotal = sumaPares + sumaImpares;
+    let respuesta;
+    if (sumaTotal % 10 == 0) {
+      respuesta = true;
+    } else respuesta = false;
+    
+    return respuesta;
+    },
+  
+    maskify: function (x) {
+    let lengthCard = x.length;
+    let mask = lengthCard -4
+    let hidden = x.slice(mask,lengthCard);
+    let maskOfCard;
+    if(lengthCard > 5){
+      maskOfCard = "#".repeat(mask) + hidden;
+     } else {
+     maskOfCard = x;
+    }
+    return maskOfCard;
+  }
+}
+export default validator;
